228cdf993127d31c714eb406eb940c00169b4441
John Kueh <yoongfook@gmail.com>
2019-10-16 17:07:38 +1100

    Can connect to aurora

--- api/lib/dbConnection.ts
+++ api/lib/dbConnection.ts
@@ -1,17 +1,29 @@
-import { createConnection, ConnectionOptions } from "typeorm";
+import { createConnection } from "typeorm";
 import { Recipe } from "../data/entities/recipe";
 
 export const createOrmConnection = () => {
-  console.log(process.env);
-  const ormConfig: ConnectionOptions = {
+  const entities = [Recipe];
+
+  if (process.env.STAGE === "production") {
+    return createConnection({
+      type: process.env.TYPEORM_TYPE,
+      database: process.env.TYPEORM_DATABASE!,
+      secretArn: process.env.TYPEORM_AURORA_SECRET_ARN!,
+      resourceArn: process.env.TYPEORM_AURORA_RESOURCE_ARN!,
+      region: process.env.TYPEORM_AURORA_REGION!,
+      synchronize: true,
+      logging: true
+      entities
+    });
+  }
+
+  return createConnection({
     type: process.env.TYPEORM_TYPE,
     port: process.env.TYPEORM_PORT!,
     host: process.env.TYPEORM_HOST!,
     database: process.env.TYPEORM_DATABASE!,
     synchronize: process.env.TYPEORM_SYNCHRONIZE!,
     logging: process.env.TYPEORM_LOGGING!,
-    entities: [Recipe]
-  };
-
-  return createConnection(ormConfig);
+    entities
+  });
 };
