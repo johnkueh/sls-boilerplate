1bf6774a6158926b2ffbfa1572d68223bbb5c602
John Kueh <yoongfook@gmail.com>
2019-10-16 16:18:12 +1100

    Refactored config to use env var

--- api/lib/dbConnection.ts
+++ api/lib/dbConnection.ts
@@ -2,14 +2,15 @@ import { createConnection, ConnectionOptions } from "typeorm";
 import { Recipe } from "../data/entities/recipe";
 
 export const createOrmConnection = () => {
+  console.log(process.env);
   const ormConfig: ConnectionOptions = {
-    type: "postgres",
-    port: 5432,
+    type: process.env.TYPEORM_TYPE,
+    port: process.env.TYPEORM_PORT!,
     host: process.env.TYPEORM_HOST!,
     database: process.env.TYPEORM_DATABASE!,
-    entities: [Recipe],
-    synchronize: true,
-    logging: true
+    synchronize: process.env.TYPEORM_SYNCHRONIZE!,
+    logging: process.env.TYPEORM_LOGGING!,
+    entities: [Recipe]
   };
 
   return createConnection(ormConfig);
